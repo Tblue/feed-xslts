01abac26a07aa599fafcf0941105c43cbf6d944f
Tilman Blumenbach <tilman+git@ax86.net>
2013-10-30 22:46:06 +0100

    tidy: Output numeric instead of named entities.
    Prevents the XML parser from choking on undefined &nbsp; entities.

--- tools/tidy.sh
+++ tools/tidy.sh
@@ -2,4 +2,4 @@
 # See the file COPYING in this distribution
 # for details on the license of this file.
 
-tidy -f /dev/null -miq -asxml -utf8 "${1:?Please specify file to tidy.}"
+tidy -f /dev/null -minq -asxml -utf8 "${1:?Please specify file to tidy.}"
